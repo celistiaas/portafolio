ed2a125f96e4327c24dd20fa198fbdf76d78e829
Celistia Azcarate <157442706+celistiaas@users.noreply.github.com>
2024-06-20 19:06:19 -0400

    Add files via upload

---
+++ validacion.js
@@ -0,0 +1,4 @@
+const formContacto = document.querySellector("form")
+const inputs = document.querySelectorAll("#contacto input");
+const mensaje = document.getElementById("mensaje");
+const botonEnviar = document.getElementById("botonEnviar");
